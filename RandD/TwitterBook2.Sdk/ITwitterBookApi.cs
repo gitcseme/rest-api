77cdb1d8f446f30385e16c3956d86b13bc92c984
kashuvo <shuvo_CSE23@yahoo.com>
2020-11-21 04:56:29 +0600

    SDK project is working with Refit

---
+++ RandD/TwitterBook2.Sdk/ITwitterBookApi.cs
@@ -0,0 +1,16 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterBook2.Contracts.V1.Responses;
+
+namespace TwitterBook2.Sdk
+{
+    [Headers("Authorization: Bearer")]
+    public interface ITwitterBookApi
+    {
+        [Get("/api/v1/posts")]
+        Task<ApiResponse<List<PostResponse>>> GetAllAsync();
+    }
+}
